8e2fcccec959b356825283b09740cb89dc1d4f50
Prime <dahmeprime@gmail.com>
2023-05-19 10:33:59 +0100

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Edu from "./images/edu.png"
 
 
 function Navbar(){
@@ -6,7 +7,7 @@ function Navbar(){
     <nav class="navbar navbar-expand-lg mt-3 bg-body-tertiary">
       <div class="container-fluid">
                
-        <a class="navbar-brand" href="/"> Edustipend</a>
+        <a class="navbar-brand" href="/"><img src={Edu}/> Edustipend</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
